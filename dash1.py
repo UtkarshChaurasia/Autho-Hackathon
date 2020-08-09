ba193afb359e79683f54cd7c19c1d1c5b60d0708
UtkarshChaurasia <msdianuc07@gmail.com>
2020-08-10 01:43:50 +0530

    readme and images

--- dash1.py
+++ dash1.py
@@ -82,7 +82,7 @@ app.layout=html.Div(children=[
                                         }}
             ),
         html.H3('PM, also known as particle pollution, is a complex mixture of air-borne particles and liquid droplets composed of acids (such as nitrates and sulfates), ammonium, water, black (or "elemental") carbon, organic chemicals, metals, and soil (crustal) material.'),
-        html.Img(src=('covidimage.jpg'))
+        html.Img(src=('images/covidimage.jpg'))
 ])
 
 
